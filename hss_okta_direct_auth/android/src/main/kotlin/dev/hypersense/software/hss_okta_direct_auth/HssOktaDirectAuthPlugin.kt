e854f4b3a39bf4009c8afdf3786892418852e6df
aldrin.francisco@hypersense-software.com <aldrin.francisco@hypersense-software.com>
2023-09-27 19:11:46 +0800

    Update HssOktaDirectAuthPlugin.kt

--- hss_okta_direct_auth/android/src/main/kotlin/dev/hypersense/software/hss_okta_direct_auth/HssOktaDirectAuthPlugin.kt
+++ hss_okta_direct_auth/android/src/main/kotlin/dev/hypersense/software/hss_okta_direct_auth/HssOktaDirectAuthPlugin.kt
@@ -1,19 +1,50 @@
+import com.okta.authfoundation.client.OidcClientResult
+import com.okta.authfoundationbootstrap.CredentialBootstrap
+import com.okta.oauth2.ResourceOwnerFlow.Companion.createResourceOwnerFlow
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import kotlinx.coroutines.runBlocking
+import java.lang.Exception
+import kotlin.coroutines.*
+
 
 class HssOktaDirectAuthPlugin : HssOktaDirectAuthPluginApi, FlutterPlugin {
-
+    
     override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        TODO("Not yet implemented")
+        HssOktaDirectAuthPluginApi.Companion.setUp(binding.binaryMessenger,this)
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        TODO("Not yet implemented")
+        HssOktaDirectAuthPluginApi.Companion.setUp(binding.binaryMessenger,null)
+
+
     }
     override fun signInWithCredentials(
         request: HssOktaDirectAuthRequest,
         callback: (Result<HssOktaDirectAuthResult?>) -> Unit
     ) {
-        TODO("Not yet implemented")
+        runBlocking {
+            var flow = CredentialBootstrap.oidcClient.createResourceOwnerFlow()
+            when(val res = flow.start(request.username,request.password)){
+                is OidcClientResult.Error -> {
+                    throw Exception(res.exception)
+                }
+
+                is OidcClientResult.Success -> {
+                    CredentialBootstrap.defaultCredential().storeToken(token = res.result)
+                    callback.invoke(Result.success(HssOktaDirectAuthResult(
+                        result = DirectAuthResult.SUCCESS,
+                        error = null,
+                        id = res.result.idToken,
+                        token = res.result.deviceSecret,
+                        issuedAt = null,
+                        tokenType = res.result.tokenType,
+                        accessToken = res.result.accessToken,
+                        scope = res.result.scope,
+                        refreshToken = res.result.accessToken
+                    )))
+                }
+            }
+        }
     }
 
     override fun mfaOtpSignInWithCredentials(
