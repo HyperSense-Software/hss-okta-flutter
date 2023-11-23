ff17cc5391b84f7423ba26d4b4e198c2a6e24576
aldrin.francisco@designli.co <aldrin.francisco@designli.co>
2023-11-23 15:40:13 +0800

    removed uneeded interface

--- hss_okta_flutter/lib/src/web/hss_okta_flutter_web.dart
+++ hss_okta_flutter/lib/src/web/hss_okta_flutter_web.dart
@@ -1,11 +1,10 @@
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'package:hss_okta_flutter/src/web/interfaces/hss_okta_flutter_web_interface.dart';
+
 import 'package:hss_okta_flutter/src/web/hss_okta_flutter_web_platform_interface.dart';
 import 'package:hss_okta_flutter/src/web/js/hss_okta_js_external.dart';
 import 'package:js/js_util.dart';
 
-class HssOktaFlutterWeb extends HssOktaFlutterWebPlatformInterface
-    implements HssOktaFlutterWebInterface {
+class HssOktaFlutterWeb extends HssOktaFlutterWebPlatformInterface {
   static void registerWith(Registrar registrar) {
     HssOktaFlutterWebPlatformInterface.instance = HssOktaFlutterWeb();
   }
@@ -18,7 +17,7 @@ class HssOktaFlutterWeb extends HssOktaFlutterWebPlatformInterface
   OktaAuth? get oktaAuth => _auth;
 
   /// Initialize the Okta client with the provided configuration.
-  @override
+
   Future<void> initializeClient({required OktaConfig oktaConfig}) async {
     _auth = OktaAuth(oktaConfig);
   }
@@ -27,7 +26,7 @@ class HssOktaFlutterWeb extends HssOktaFlutterWebPlatformInterface
   ///  After a successful authentication, the browser will be redirected to the configured redirectUri.
   ///  The authorization code, access, or ID Tokens will be available as parameters appended to this URL.
   ///  Values will be returned in either the search query or hash fragment portion of the URL depending on the responseMode
-  @override
+
   Future<void> startRedirectAuthentication({AuthorizeOptions? options}) async {
     await promiseToFuture<void>(_auth.token.getWithRedirect(options));
   }
@@ -46,7 +45,7 @@ class HssOktaFlutterWeb extends HssOktaFlutterWebPlatformInterface
   /// In case access token is a part of OIDC flow response, its hash will be checked against ID token's at_hash claim.
   ///
   ///The state string which was passed to getWithRedirect will be also be available on the response.
-  @override
+
   Future<TokenResponse> parseFromUrl() async {
     final res =
         await promiseToFuture<TokenResponse>(_auth.token.parseFromUrl());
@@ -55,7 +54,7 @@ class HssOktaFlutterWeb extends HssOktaFlutterWebPlatformInterface
   }
 
   /// Create token with a popup.
-  @override
+
   Future<TokenResponse> startPopUpAuthentication(
       {AuthorizeOptions? options}) async {
     final res =
@@ -64,14 +63,14 @@ class HssOktaFlutterWeb extends HssOktaFlutterWebPlatformInterface
   }
 
   /// Returns a new token if the Okta session is still valid.
-  @override
+
   Future<TokenResponse> renew(String tokenToRenew) async {
     final res = await promiseToFuture(_auth.token.renew(tokenToRenew));
     return res;
   }
 
   /// Decode a token.
-  @override
+
   Future<TokenResponse> decode(String idTokenString) async {
     final res = await promiseToFuture(_auth.token.decode(idTokenString));
     return res;
@@ -82,7 +81,7 @@ class HssOktaFlutterWeb extends HssOktaFlutterWebPlatformInterface
   ///
   ///[key]- Unique key to store the token in the tokenManager. This is used later when you want to get, delete, or renew the token.
   /// [token] - Token object that will be added
-  @override
+
   void addToken(String key, Token token) async {
     await promiseToFuture(_auth.tokenManager.add(key, token));
   }
@@ -90,7 +89,7 @@ class HssOktaFlutterWeb extends HssOktaFlutterWebPlatformInterface
   ///Get a [Token] that you have previously added to the tokenManager with the given key.
   /// The [Token] object will be returned if it exists in storage. Tokens will be removed from storage if they have expired and autoRenew is false or if there was an error while renewing the token.
   /// The [TokenManager] will emit a removed event when tokens are removed.
-  @override
+
   Future<Token> getToken(String key) async {
     final res = await promiseToFuture(_auth.tokenManager.get(key));
     return res;
@@ -98,42 +97,40 @@ class HssOktaFlutterWeb extends HssOktaFlutterWebPlatformInterface
 
   ///Remove a [Token] from the tokenManager with the given key.
   ///[key] - Key for the token you want to remove
-  @override
+
   void removeToken(String key) async {
     _auth.tokenManager.remove(key);
   }
 
   ///Remove all tokens from the tokenManager.
-  @override
+
   void clearTokens() async {
     _auth.tokenManager.clear();
   }
 
   /// Returns true if the current page is a redirect from the authorization server.
-  @override
+
   bool isRedirect() {
     return _auth.isRedirect();
   }
 
   /// Check window.location to verify if the app is in OAuth callback state or not. This function is synchronous and returns true or false.
-  @override
+
   String getAccessToken() {
     return _auth.getAccessToken();
   }
 
   /// Returns the id token string retrieved if it exists.
-  @override
+
   String getIdToken() {
     return _auth.getIdToken();
   }
 
-  @override
   Future<TokenResponse> getUserInfo({required String accessToken}) {
     // TODO: implement getUserInfo
     throw UnimplementedError();
   }
 
-  @override
   Future<TokenResponse> renewToken({required String refreshToken}) {
     // TODO: implement renewToken
     throw UnimplementedError();
