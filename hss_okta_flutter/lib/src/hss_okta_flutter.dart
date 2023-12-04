9207e1066b82abbfe61b190582f89d0121693b17
Vince Ramces Oliveros <vince.oliveros@hypersense-software.com>
2023-12-04 14:31:56 +0800

    feat: improve documentation with example

--- hss_okta_flutter/lib/src/hss_okta_flutter.dart
+++ hss_okta_flutter/lib/src/hss_okta_flutter.dart
@@ -1,93 +1,122 @@
-import 'enums/sign_in_factors.dart';
-import 'generated/hss_okta_flutter.g.dart';
-import 'hss_okta_platform_interface.dart';
+import 'package:hss_okta_flutter/hss_okta_flutter.dart';
+import 'package:hss_okta_flutter/src/hss_okta_platform_interface.dart';
 
+/// {@template hss_okta_flutter}
+/// A Flutter plugin for Okta Authentication
+/// {@endtemplate}
 class HssOktaFlutter {
+  final _instance = HssOktaFlutterPlatformInterface.instance;
+
   /// Starts the Direct Authentication /Resource Owner Flow
   ///
-  /// factors is either [AuthenticationFactor.otp] or [AuthenticationFactor.oob] or both.
-  /// factors is not needed for Android as it cannot handle Directauthentication MFA
+  /// factors is either [AuthenticationFactor.otp] or
+  /// [AuthenticationFactor.oob] or both.
+  /// factors is not needed for Android as it cannot handle
+  /// Directauthentication MFA.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// Future<void> startDirectAuthenticationFlow() async {
+  ///   final result = await HssOktaFlutter().startDirectAuthenticationFlow(
+  ///               email: 'test@test.com',
+  ///               password: 'your_password_text',
+  ///               factors: [OktaSignInFactor.otp]);
+  ///
+  ///   if (result != null) {
+  ///     // do something with result
+  ///   }
+  /// }
+  /// ```
+  ///
   /// Throws an [Exception] When used on Android
-  Future<AuthenticationResult?> startDirectAuthenticationFlow(
-      {required String email,
-      required String password,
-      required List<OktaSignInFactor> factors}) async {
-    var factorString = factors.map((e) => e.factor).toList();
+  Future<AuthenticationResult?> startDirectAuthenticationFlow({
+    required String email,
+    required String password,
+    required List<OktaSignInFactor> factors,
+  }) async {
+    final factorString = factors.map((e) => e.factor).toList();
 
-    var result = await HssOktaFlutterPlatformInterface.instance
-        .startDirectAuthenticationFlow(DirectAuthRequest(
-            username: email, password: password, factors: factorString));
+    final result = await _instance.startDirectAuthenticationFlow(
+      DirectAuthRequest(
+        username: email,
+        password: password,
+        factors: factorString,
+      ),
+    );
 
     return result;
   }
 
   /// Revokes the default Credential stored on the device
   Future<bool> revokeDefaultToken() async {
-    var result =
-        await HssOktaFlutterPlatformInterface.instance.revokeDefaultToken();
+    final result = await _instance.revokeDefaultToken();
 
     return result;
   }
 
   /// Refreshes the default Credential stored on the device
   Future<bool> refreshDefaultToken() async {
-    var result =
-        await HssOktaFlutterPlatformInterface.instance.refreshDefaultToken();
+    final result = await _instance.refreshDefaultToken();
 
     return result;
   }
 
   /// tries to get the default Credential stored on the device
   Future<AuthenticationResult?> getCredential() async {
-    var result = await HssOktaFlutterPlatformInterface.instance.getCredential();
+    final result = await _instance.getCredential();
 
     return result;
   }
 
-  /// To be used with DirectAuthentication Flow
-  /// returns [AuthenticationResult] which contains the [OktaToken] and [UserInfo]
+  /// To be used with DirectAuthentication Flow returns [AuthenticationResult]
+  /// which contains the [OktaToken] and [UserInfo]
   ///
   /// This can only be used for iOS and when using Identity Engine
-  /// [OktaSignInFactor.otp] or [OktaSignInFactor.oob] needs to be enabled on the Okta App.
+  /// [OktaSignInFactor.otp] or [OktaSignInFactor.oob] needs to be enabled
+  /// on the Okta App.
   Future<AuthenticationResult?> mfaOtpSignInWithCredentials(String otp) async {
-    var result = await HssOktaFlutterPlatformInterface.instance
-        .continueDirectAuthenticationMfaFlow(otp);
+    final result = await _instance.continueDirectAuthenticationMfaFlow(otp);
 
     return result;
   }
 
   /// Starts the Device Authorization flow
   ///
-  /// The Device Authorization Flow is designed for Internet connected devices that either lack a browser to perform a user-agent based authorization,
-  ///  or are input constrained to the extent that requiring the user to input text in order to authenticate during the authorization flow is impractical.
+  /// The Device Authorization Flow is designed for Internet connected devices
+  /// that either lack a browser to perform a user-agent based authorization,
+  /// or are input constrained to the extent that requiring the user to input
+  /// text in order to authenticate during the authorization flow is
+  /// impractical.
   ///
-  /// returns [DeviceAuthorizationSession] which contains the DeviceCode and VerificationUri
+  /// returns [DeviceAuthorizationSession] which contains
+  /// the DeviceCode and VerificationUri
   /// Users will need to input the DeviceCode and VerificationUri on a browser
   ///
-  /// Call on [resumeDeviceAuthorizationFlow] to continue the flow and access the [AuthenticationResult]
+  /// Call on [resumeDeviceAuthorizationFlow] to continue the flow and access
+  /// the [AuthenticationResult]
   Future<DeviceAuthorizationSession?> startDeviceAuthorizationFlow() async {
-    var result = await HssOktaFlutterPlatformInterface.instance
-        .startDeviceAuthorizationFlow();
+    final result = await _instance.startDeviceAuthorizationFlow();
     return result;
   }
 
-  /// Continues the DeviceAuthorization flow, this should be called once the user has inputted the DeviceCode and VerificationUri
-  ///  on a browser
+  /// Continues the DeviceAuthorization flow, this should be called once the
+  /// user has inputted the DeviceCode and VerificationUri on a browser
   Future<AuthenticationResult?> resumeDeviceAuthorizationFlow() async {
-    var result = await HssOktaFlutterPlatformInterface.instance
-        .resumeDeviceAuthorizationFlow();
+    final result = await _instance.resumeDeviceAuthorizationFlow();
     return result;
   }
 
   /// Starts the Token Exchange flow
-  /// The Token Exchange Flow exchanges an ID Token and a Device Secret for a new set of tokens.
+  /// The Token Exchange Flow exchanges an ID Token and a Device Secret
+  /// for a new set of tokens.
+  ///
   /// [deviceSecret] and [idToken] are obtained from the and Existing Session
   Future<AuthenticationResult?> startTokenExchangeFlow({
     required String deviceSecret,
     required String idToken,
   }) async {
-    var result =
-        await HssOktaFlutterPlatformInterface.instance.startTokenExchangeFlow(
+    final result = await _instance.startTokenExchangeFlow(
       deviceSecret: deviceSecret,
       idToken: idToken,
     );
--- hss_okta_flutter/lib/src/hss_okta_flutter_channel.dart
+++ hss_okta_flutter/lib/src/hss_okta_flutter_channel.dart
@@ -1,43 +1,45 @@
-import 'generated/hss_okta_flutter.g.dart';
-import 'hss_okta_platform_interface.dart';
+import 'package:hss_okta_flutter/src/generated/hss_okta_flutter.g.dart';
+import 'package:hss_okta_flutter/src/hss_okta_platform_interface.dart';
 
 class HssOktaFlutterChannel extends HssOktaFlutterPlatformInterface {
   final _api = HssOktaFlutterPluginApi();
 
   @override
   Future<AuthenticationResult> startDirectAuthenticationFlow(
-      DirectAuthRequest request) async {
-    var res = await _api.startDirectAuthenticationFlow(request);
-    if (res == null) throw Exception("Null result from signInWithCredentials");
+    DirectAuthRequest request,
+  ) async {
+    final res = await _api.startDirectAuthenticationFlow(request);
+    if (res == null) throw Exception('Null result from signInWithCredentials');
     return res;
   }
 
   @override
   Future<bool> revokeDefaultToken() async {
-    var res = await _api.revokeDefaultToken();
-    if (res == null) throw Exception("Null result from revokeDefaultToken");
+    final res = await _api.revokeDefaultToken();
+    if (res == null) throw Exception('Null result from revokeDefaultToken');
     return res;
   }
 
   @override
   Future<bool> refreshDefaultToken() async {
-    var res = await _api.refreshDefaultToken();
-    if (res == null) throw Exception("Null result from refreshDefaultToken");
+    final res = await _api.refreshDefaultToken();
+    if (res == null) throw Exception('Null result from refreshDefaultToken');
     return res;
   }
 
   @override
   Future<AuthenticationResult?> getCredential() async {
-    var res = await _api.getCredential();
+    final res = await _api.getCredential();
     return res;
   }
 
   @override
   Future<AuthenticationResult?> continueDirectAuthenticationMfaFlow(
-      String otp) async {
-    var res = await _api.continueDirectAuthenticationMfaFlow(otp);
+    String otp,
+  ) async {
+    final res = await _api.continueDirectAuthenticationMfaFlow(otp);
     if (res == null) {
-      throw Exception("Null result from mfaOtpSignInWithCredentials");
+      throw Exception('Null result from mfaOtpSignInWithCredentials');
     }
 
     return res;
@@ -45,9 +47,9 @@ class HssOktaFlutterChannel extends HssOktaFlutterPlatformInterface {
 
   @override
   Future<DeviceAuthorizationSession?> startDeviceAuthorizationFlow() async {
-    var res = await _api.startDeviceAuthorizationFlow();
+    final res = await _api.startDeviceAuthorizationFlow();
     if (res == null) {
-      throw Exception("Null result from startDeviceAuthorizationFlow");
+      throw Exception('Null result from startDeviceAuthorizationFlow');
     }
 
     return res;
@@ -55,7 +57,7 @@ class HssOktaFlutterChannel extends HssOktaFlutterPlatformInterface {
 
   @override
   Future<AuthenticationResult?> resumeDeviceAuthorizationFlow() async {
-    var res = await _api.resumeDeviceAuthorizationFlow();
+    final res = await _api.resumeDeviceAuthorizationFlow();
 
     return res;
   }
@@ -65,7 +67,7 @@ class HssOktaFlutterChannel extends HssOktaFlutterPlatformInterface {
     required String deviceSecret,
     required String idToken,
   }) async {
-    var res = await _api.startTokenExchangeFlow(deviceSecret, idToken);
+    final res = await _api.startTokenExchangeFlow(deviceSecret, idToken);
 
     return res;
   }
