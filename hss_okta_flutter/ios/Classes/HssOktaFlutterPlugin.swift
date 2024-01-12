2f231c7562dfff68d93cea31c990855f408ea1f8
Shahzaib Ali Sherazi <shahzaib.ali.sherazi@mvpfactory.co>
2024-01-13 00:11:58 +0500

    Update HssOktaFlutterPlugin.swift

--- hss_okta_flutter/ios/Classes/HssOktaFlutterPlugin.swift
+++ hss_okta_flutter/ios/Classes/HssOktaFlutterPlugin.swift
@@ -63,7 +63,8 @@ public class HssOktaFlutterPlugin: NSObject, FlutterPlugin,HssOktaFlutterPluginA
                             
                         case .mfaRequired(_):
                             completion(.success(AuthenticationResult(result: DirectAuthenticationResult.mfaRequired)))
-                        case .bindingUpdate(_): break
+                        default:
+                            break
                             
                         }
                     }else{
